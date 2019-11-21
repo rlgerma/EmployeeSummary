ce74d80be4354992ef906e29a3a5b8f73fc1be60
Richard Germaine <rlgerma92@outlook.com>
2019-11-20 21:29:18 -0700

    still working on app portion

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
+// vars const 
 const inquirer = require('inquirer');
 const fs = require('fs');
 const employee = require('./lib/Employee');
-const intern = require('./lib/Intern').default;
+const intern = require('./lib/Intern');
 const engineer = require('./lib/Engineer');
 const manager = require('./lib/Manager');
 const questions = require('./lib/ques');
 
-const summary = () => prompt(questions);
+const nameDb = [];
+
+const summary = () =>{
+   console.log("Welcome to Team Builder")
+    
+   inquirer
+   .prompt(Ques.qs)
+   .then(function(who){
+      
+   })
+   inquirer
+   .prompt(Ques.title)
+   .then(function(what){
+
+   })
+   
+}   
+summary();
